0c6aeb122cf56e895c36e65ccfe68b6d7a0ccfe0
Qiqi Wang <qiqi.wang@gmail.com>
2016-04-10 10:30:26 -0400

    Update pascals.js

--- pascals.js
+++ pascals.js
@@ -6,7 +6,7 @@ function binomial(n, k) {
     var coeff = 1;  
     for (var x = n-k+1; x <= n; x++) coeff *= x;  
     for (x = 1; x <= k; x++) coeff /= x;  
-    return coeff;  
+    return Math.round(coeff);  
 }
 
 function box_keyup(evt) {
